ea638daa972f3093d306b5ecacb13cc7753492d1
Jason <jasonyata@gmail.com>
2021-04-10 16:24:18 -0700

    added netlify forms to contact forms

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -5,202 +5,230 @@ import axios from "axios";
 import "./form.scss";
 
 class Form extends Component {
-  state = {
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-    error: false,
-    displaySuccess: false,
-    visibility: "fadeOutClass",
-  };
+    state = {
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+        error: false,
+        displaySuccess: false,
+        visibility: "fadeOutClass",
+    };
 
-  handleFormSubmit = async (event) => {
-    const { name, email, subject, message } = this.state;
-    event.preventDefault();
-    await axios({
-      method: "POST",
-      url: "/api/mail_handler.php",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      data: `name=${name}&email=${email}&subject=${subject}&message=${message}`,
-    }).then((resp) => {
-      if (resp.data.success === true) {
-        this.setState({
-          name: "",
-          email: "",
-          subject: "",
-          message: "",
-          displaySuccess: true,
-          visibility: "fadeInClass",
+    handleFormSubmit = async event => {
+        const { name, email, subject, message } = this.state;
+        event.preventDefault();
+        await axios({
+            method: "POST",
+            url: "/api/mail_handler.php",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            data: `name=${name}&email=${email}&subject=${subject}&message=${message}`,
+        }).then(resp => {
+            if (resp.data.success === true) {
+                this.setState({
+                    name: "",
+                    email: "",
+                    subject: "",
+                    message: "",
+                    displaySuccess: true,
+                    visibility: "fadeInClass",
+                });
+                setTimeout(
+                    function () {
+                        this.setState({
+                            visibility: "fadeOutClass",
+                        });
+                    }.bind(this),
+                    4000
+                );
+                setTimeout(
+                    function () {
+                        this.setState({
+                            displaySuccess: false,
+                        });
+                    }.bind(this),
+                    5500
+                );
+            } else {
+                this.setState({
+                    error: true,
+                    visibility: "fadeInClass",
+                });
+                setTimeout(
+                    function () {
+                        this.setState({
+                            visibility: "fadeOutClass",
+                        });
+                    }.bind(this),
+                    4000
+                );
+                setTimeout(
+                    function () {
+                        this.setState({
+                            error: false,
+                        });
+                    }.bind(this),
+                    5500
+                );
+            }
         });
-        setTimeout(
-          function () {
-            this.setState({
-              visibility: "fadeOutClass",
-            });
-          }.bind(this),
-          4000
-        );
-        setTimeout(
-          function () {
-            this.setState({
-              displaySuccess: false,
-            });
-          }.bind(this),
-          5500
-        );
-      } else {
-        this.setState({
-          error: true,
-          visibility: "fadeInClass",
-        });
-        setTimeout(
-          function () {
-            this.setState({
-              visibility: "fadeOutClass",
-            });
-          }.bind(this),
-          4000
-        );
-        setTimeout(
-          function () {
-            this.setState({
-              error: false,
-            });
-          }.bind(this),
-          5500
-        );
-      }
-    });
-  };
+    };
 
-  render() {
-    const {
-      name,
-      email,
-      subject,
-      message,
-      error,
-      displaySuccess,
-      visibility,
-    } = this.state;
+    render() {
+        const {
+            name,
+            email,
+            subject,
+            message,
+            error,
+            displaySuccess,
+            visibility,
+        } = this.state;
 
-    return (
-      <Fade>
-        <form
-          id="contact-form"
-          className="primary-container"
-          method="POST"
-          netlify
-        >
-          <div className="container special-form-container">
-            <div className="row form-header-row">
-              <div className="col s12">
-                <h2 className="form-main-text">Contact</h2>
-                <h3 className="form-description">Get in touch with us</h3>
-              </div>
-            </div>
-            <Fade bottom>
-              <div className="row">
-                <div className="col s12">
-                  <div className="row first-form-row">
-                    <div className="input-field col s12 m6">
-                      <i className="material-icons prefix">account_circle</i>
-                      <input
-                        name="name"
-                        id="name"
-                        type="text"
-                        value={name}
-                        onChange={(e) =>
-                          this.setState({ name: e.target.value })
-                        }
-                      />
-                      <label htmlFor="name">Name</label>
+        return (
+            <Fade>
+                <form
+                    id="contact-form"
+                    className="primary-container"
+                    method="POST"
+                    data-netlify="true"
+                    name="contact"
+                >
+                    <input type="hidden" name="form-name" value="contact" />
+                    <div className="container special-form-container">
+                        <div className="row form-header-row">
+                            <div className="col s12">
+                                <h2 className="form-main-text">Contact</h2>
+                                <h3 className="form-description">
+                                    Get in touch with us
+                                </h3>
+                            </div>
+                        </div>
+                        <Fade bottom>
+                            <div className="row">
+                                <div className="col s12">
+                                    <div className="row first-form-row">
+                                        <div className="input-field col s12 m6">
+                                            <i className="material-icons prefix">
+                                                account_circle
+                                            </i>
+                                            <input
+                                                name="name"
+                                                id="name"
+                                                type="text"
+                                                value={name}
+                                                onChange={e =>
+                                                    this.setState({
+                                                        name: e.target.value,
+                                                    })
+                                                }
+                                            />
+                                            <label htmlFor="name">Name</label>
+                                        </div>
+                                        <div className="input-field col s12 m6 email-input-field">
+                                            <i className="material-icons prefix">
+                                                email
+                                            </i>
+                                            <input
+                                                name="email"
+                                                id="email"
+                                                type="email"
+                                                value={email}
+                                                className="validate"
+                                                onChange={e =>
+                                                    this.setState({
+                                                        email: e.target.value,
+                                                    })
+                                                }
+                                            />
+                                            <label htmlFor="email">Email</label>
+                                            <span
+                                                className="helper-text"
+                                                data-error="Please enter a valid email address"
+                                                data-success="✓"
+                                            ></span>
+                                        </div>
+                                    </div>
+                                    <div className="row third-form-row">
+                                        <div className="input-field col s12">
+                                            <i className="material-icons prefix">
+                                                subject
+                                            </i>
+                                            <input
+                                                name="subject"
+                                                id="subject"
+                                                type="text"
+                                                value={subject}
+                                                onChange={e =>
+                                                    this.setState({
+                                                        subject: e.target.value,
+                                                    })
+                                                }
+                                            />
+                                            <label htmlFor="subject">
+                                                Subject
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="input-field col s12">
+                                            <i className="material-icons prefix">
+                                                mode_edit
+                                            </i>
+                                            <textarea
+                                                name="message"
+                                                id="message"
+                                                className="materialize-textarea custom-textarea"
+                                                value={message}
+                                                onChange={e =>
+                                                    this.setState({
+                                                        message: e.target.value,
+                                                    })
+                                                }
+                                            ></textarea>
+                                            <label htmlFor="message">
+                                                Message
+                                            </label>
+                                        </div>
+                                    </div>
+                                    {error && (
+                                        <div
+                                            className={`error-message-container ${visibility}`}
+                                        >
+                                            <h3 className="error-message">
+                                                Oops, something went wrong
+                                            </h3>
+                                        </div>
+                                    )}
+                                    {displaySuccess && (
+                                        <div
+                                            className={`success-message-container ${visibility}`}
+                                        >
+                                            <h3 className="success-message">
+                                                Your message has been sent!
+                                                We'll get back to you shortly.
+                                            </h3>
+                                        </div>
+                                    )}
+                                    <div className="row">
+                                        <div className="col s12 form-btn-container">
+                                            <button
+                                                type="submit"
+                                                className="waves-effect waves-light btn custom-form-btn"
+                                                // onClick={(e) => this.handleFormSubmit(e)}
+                                            >
+                                                Submit
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Fade>
                     </div>
-                    <div className="input-field col s12 m6 email-input-field">
-                      <i className="material-icons prefix">email</i>
-                      <input
-                        name="email"
-                        id="email"
-                        type="email"
-                        value={email}
-                        className="validate"
-                        onChange={(e) =>
-                          this.setState({ email: e.target.value })
-                        }
-                      />
-                      <label htmlFor="email">Email</label>
-                      <span
-                        className="helper-text"
-                        data-error="Please enter a valid email address"
-                        data-success="✓"
-                      ></span>
-                    </div>
-                  </div>
-                  <div className="row third-form-row">
-                    <div className="input-field col s12">
-                      <i className="material-icons prefix">subject</i>
-                      <input
-                        name="subject"
-                        id="subject"
-                        type="text"
-                        value={subject}
-                        onChange={(e) =>
-                          this.setState({ subject: e.target.value })
-                        }
-                      />
-                      <label htmlFor="subject">Subject</label>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <i className="material-icons prefix">mode_edit</i>
-                      <textarea
-                        name="message"
-                        id="message"
-                        className="materialize-textarea custom-textarea"
-                        value={message}
-                        onChange={(e) =>
-                          this.setState({ message: e.target.value })
-                        }
-                      ></textarea>
-                      <label htmlFor="message">Message</label>
-                    </div>
-                  </div>
-                  {error && (
-                    <div className={`error-message-container ${visibility}`}>
-                      <h3 className="error-message">
-                        Oops, something went wrong
-                      </h3>
-                    </div>
-                  )}
-                  {displaySuccess && (
-                    <div className={`success-message-container ${visibility}`}>
-                      <h3 className="success-message">
-                        Your message has been sent! We'll get back to you
-                        shortly.
-                      </h3>
-                    </div>
-                  )}
-                  <div className="row">
-                    <div className="col s12 form-btn-container">
-                      <button
-                        type="submit"
-                        className="waves-effect waves-light btn custom-form-btn"
-                        // onClick={(e) => this.handleFormSubmit(e)}
-                      >
-                        Submit
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
+                </form>
             </Fade>
-          </div>
-        </form>
-      </Fade>
-    );
-  }
+        );
+    }
 }
 
 export default Form;
--- src/components/landing_header/landing_carousel.js
+++ src/components/landing_header/landing_carousel.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import carouselPhoto1 from '../../assets/images/landing-background-1-edit.jpg';
-import carouselPhoto2 from '../../assets/images/steely1-dark.jpg';
-import carouselPhoto3 from '../../assets/images/steely3-dark.jpg';
+import React, { Component } from "react";
+import carouselPhoto1 from "../../assets/images/landing-background-1-edit-min.jpg";
+import carouselPhoto2 from "../../assets/images/landing-background-3-edit-min.jpg";
+import carouselPhoto3 from "../../assets/images/steely3-dark-min.jpg";
 import "./landing_header.scss";
 
 class Carousel extends Component {
@@ -10,22 +10,39 @@ class Carousel extends Component {
     }
 
     render() {
-
         return (
-            <div id="slider" ref={(element) => {this.slider = element}} className="slider">
+            <div
+                id="slider"
+                ref={element => {
+                    this.slider = element;
+                }}
+                className="slider"
+            >
                 <ul className="slides">
                     <li>
-                        <img src={carouselPhoto1} alt="stormtrooper bus" className="carousel-photo-bottom" />
+                        <img
+                            src={carouselPhoto1}
+                            alt="stormtrooper bus"
+                            className="carousel-photo-bottom"
+                        />
                     </li>
                     <li>
-                        <img src={carouselPhoto2} alt="stormtrooper bus" className="carousel-photo" />
+                        <img
+                            src={carouselPhoto2}
+                            alt="stormtrooper bus"
+                            className="carousel-photo"
+                        />
                     </li>
                     <li>
-                        <img src={carouselPhoto3} alt="another bus" className="carousel-photo" />
+                        <img
+                            src={carouselPhoto3}
+                            alt="another bus"
+                            className="carousel-photo"
+                        />
                     </li>
                 </ul>
             </div>
-        )
+        );
     }
 }
 
